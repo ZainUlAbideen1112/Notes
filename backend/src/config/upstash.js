7cf5d895443bb84c9680d36fb787d982493f32bc
Zain <zainulabideen2518@gmail.com>
2025-08-04 20:49:18 +0500

    Create Note & Homepage Updated

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -5,7 +5,7 @@ import "dotenv/config";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(3, "10s"),
+  limiter: Ratelimit.slidingWindow(20, "60s"),
 });
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
+import notesRoutes from "./routes/notesRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
@@ -10,8 +11,12 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
-
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
