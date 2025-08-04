1cbe170c4375291777aebd725d70104a1db5aa9d
Zain <zainulabideen2518@gmail.com>
2025-08-05 01:19:25 +0500

    Delete Note added

--- Frontend/src/components/NoteCard.jsx
+++ Frontend/src/components/NoteCard.jsx
@@ -1,8 +1,24 @@
 import { Link } from "react-router";
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { formatDate } from "../lib/utils";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+
+    if (!window.confirm("Are you sure")) return;
+
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter((note) => note._id !== id));
+      toast.success("Deleted");
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("Error Deleting Note");
+    }
+  };
   return (
     <Link
       to={`/note/${note._id}`}
@@ -19,7 +35,12 @@ const NoteCard = ({ note }) => {
         </div>
         <div className="flex items-center gap-1">
           <PenSquareIcon className="size-4" />
-          <button className="btn btn-ghost btn-xs text-error">
+          <button
+            className="btn btn-ghost btn-xs text-error"
+            onClick={(e) => {
+              handleDelete(e, note._id);
+            }}
+          >
             <Trash2Icon className="size-4" />
           </button>
         </div>
